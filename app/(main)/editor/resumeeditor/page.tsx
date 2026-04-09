74f0ebe04aaaa7e9420adbb336ce457f1315ff7c
swagatpadhi <litan3104@gmail.com>
2026-04-09 21:13:07 +0530

    resume editor added

--- app/(main)/editor/resumeeditor/page.tsx
+++ app/(main)/editor/resumeeditor/page.tsx
@@ -1,20 +1,36 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function ResumeEditor() {
     return (
-        <div className="bg-amber-400  w-full min-h-screen flex grow flex-col">
-            <header className="bg-amber-700 w-full text-center py-4">
+        <div className="  w-full min-h-screen flex grow flex-col">
+            <header className=" w-full text-center py-4">
                 <h1 className="font-black text-5xl">Design Your Resume</h1>
                 <p>description</p>
             </header>
-            <main>
-
+            <main className="relative grow">
+                <div className="absolute bg-amber-200 top-0 bottom-0 flex w-full">
+                    <div className="md:w-1/2 w-full bg-amber-500">
+                        left
+                    </div>
+                    <div className="grow md:border-r"/>
+                    <div className="hidden w-1/2 md:flex bg-amber-700">
+                        right
+                    </div>
+                </div>
             </main>
-            <footer className=" w-full border-t px-3">
+            <footer className=" w-full border-t px-3 py-5">
                 <div className="max-w-7xl mx-auto flex flex-wrap justify-between gap-3">
                     <div className="flex items-center gap-3">
-                        <Button variant="secondary" className="bg-slate-950 rounded-5xl text-white">previous</Button>
-                        <Button>next</Button>
+                        <Button variant="secondary" size="default">Previous</Button>
+                        <Button size="default">Next</Button>
+                    </div>
+                    <div>
+                        <Button variant="secondary" asChild>
+                            <Link href="/resume">
+                                Back to Home
+                            </Link>
+                        </Button>
                     </div>
                 </div>
             </footer>
