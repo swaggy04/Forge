7f3f2a0a00dad187e22e7e3773be89c00a3ac832
swagatpadhi <litan3104@gmail.com>
2026-04-11 00:19:58 +0530

    feat: initial commit

--- app/(main)/editor/forms/geninfoform.tsx
+++ app/(main)/editor/forms/geninfoform.tsx
@@ -1,27 +1,60 @@
+"use client";
 
 import { generalInfoSchema, generalInfoType } from "@/lib/validation";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
-export default function GenInfoForm(){
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from "@/components/ui/form";
 
-    const form = useForm<generalInfoType>({
-        resolver: zodResolver(generalInfoSchema),
-        defaultValues:{
-            title:"",
-            description:""
-        }
+import { Input } from "@/components/ui/input";
 
-    })
+export default function GenInfoForm() {
+  const form = useForm<generalInfoType>({
+    resolver: zodResolver(generalInfoSchema),
+    defaultValues: {
+      title: "",
+      description: "",
+    },
+  });
 
-    return (
-        <div className=" mx-auto space-y-6 max-w-xl">
-            <div className=" space-x-1.5 text-center">
-                <h2 className=" text-2xl font-bold">
-                    General info
-                </h2>
-                <p className=" text-sm text-muted-foreground">this will not appear in the resume</p>
-            </div>
-        </div>
-    )
+  return (
+    <div className="mx-auto space-y-6 max-w-xl">
+      <div className="space-y-1.5 text-center">
+        <h2 className="text-2xl font-bold">General info</h2>
+        <p className="text-sm text-muted-foreground">
+          this will not appear in the resume
+        </p>
+      </div>
+
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit((data) => console.log(data))}
+          className="space-y-4"
+        >
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Title</FormLabel>
+                <FormControl>
+                  <Input placeholder="Title" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <button type="submit">Submit</button>
+        </form>
+      </Form>
+    </div>
+  );
 }
