351bb8e8ba0db47f3746ee1739439f319bb4a90f
swagatpadhi <litan3104@gmail.com>
2026-04-15 23:07:15 +0530

    forms continue

---
+++ app/(main)/editor/breadcrumbs.tsx
@@ -0,0 +1,37 @@
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
+import { steps } from "./steps";
+import  React  from "react";
+
+interface BreadcrumbsProps {
+
+    currentstep:string;
+    setcurrentstep:(step:string) => void;
+
+
+}
+
+export default function Breadcrumbs({currentstep,setcurrentstep}:BreadcrumbsProps){
+    return <div>
+        <Breadcrumb>
+            <BreadcrumbList>
+                {steps.map(step =>(
+                    <React.Fragment key={step.key}>
+                        <BreadcrumbItem>
+                            {step.key === currentstep ? (
+                                <BreadcrumbPage>{step.title}</BreadcrumbPage>
+                            ):(
+                                <BreadcrumbLink asChild>
+                                    <button onClick={()=>setcurrentstep(step.key)}>
+                                        {step.title}
+                                    </button>
+                                </BreadcrumbLink>
+                            )}
+                        </BreadcrumbItem>
+                        <BreadcrumbSeparator/>
+                    </React.Fragment>
+                ))}
+            </BreadcrumbList>
+        </Breadcrumb>
+    </div>
+
+}
