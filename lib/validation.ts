06c9097636bbeabf868be1f5b84f613dcbb35d80
swagatpadhi <litan3104@gmail.com>
2026-04-20 20:42:40 +0530

    form schema are accumulated

--- lib/validation.ts
+++ lib/validation.ts
@@ -1,3 +1,4 @@
+
 import { z} from "zod";
 
 export const optionalString =z.string().trim().optional().or(z.literal(""));
@@ -32,3 +33,13 @@ export const personalInfoSchema =z.object({
 })
 
 export type personalInfoType=z.infer<typeof personalInfoSchema>
+
+export const resumeSchema = z.object({
+  ...generalInfoSchema.shape,
+  ...personalInfoSchema.shape
+})
+
+export type resumeSchemaType=Omit<z.infer<typeof resumeSchema>,"photo"> & {
+  id? :string;
+  photo?: string| File|null;
+}
