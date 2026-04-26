744e0aee4d8886891002cb0dcd8ae6e2eac991af
swagatpadhi <litan3104@gmail.com>
2026-04-26 17:23:28 +0530

    usefield array introduced

--- app/(main)/editor/forms/workexpfrom.tsx
+++ app/(main)/editor/forms/workexpfrom.tsx
@@ -2,26 +2,41 @@ import { EditorFormProps } from "@/lib/types";
 import { workExperienceSchema, workExperienceType } from "@/lib/validation";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useEffect } from "react";
-import { useForm } from "react-hook-form";
+import { useFieldArray, useForm } from "react-hook-form";
+
+export default function WorkExpForm({
+  ResumeData,
+  setResumeData,
+}: EditorFormProps) {
+  const form = useForm<workExperienceType>({
+    resolver: zodResolver(workExperienceSchema),
+    defaultValues: {
+      workexp: ResumeData.workexp || [],
+    },
+  });
+
+  useEffect(() => {
+    const { unsubscribe } = form.watch(async (values) => {
+      const isValid = await form.trigger();
+      if (!isValid) return;
+      setResumeData({
+        ...ResumeData,
+        workexp: values.workexp?.filter((exp) => exp !== undefined) || [],
+      });
+    });
+    return unsubscribe;
+  }, [form, ResumeData, setResumeData]);
+
+  const { fields, append } = useFieldArray({
+    control: form.control,
+    name: "workexp",
+  });
+
+  return <div className="mx-auto space-y-6 max-w-xl">
+        <div className="space-y-1.5 text-center">
+
+        </div>
 
 
-export default function WorkExpForm({ResumeData,setResumeData}:EditorFormProps){
-    const form = useForm<workExperienceType>({
-        resolver : zodResolver(workExperienceSchema),
-        defaultValues:{
-            workexp : ResumeData.workexp || []
-        }
-    })
-
-useEffect(() => {
-  const {unsubscribe} = form.watch(async (values) =>{
-    const isValid = await form.trigger();
-    if(!isValid) return 
-    setResumeData({...ResumeData,
-        workexp: values.workexp?.filter((exp)=> exp!==undefined ) || []
-    })
-  })
-  return unsubscribe
-  }, [form,ResumeData,setResumeData])
-  
+  </div>;
 }
