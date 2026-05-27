3cf50e4c22f467a5774b0dd2205a19168224a815
swagatpadhi <litan3104@gmail.com>
2026-05-27 17:09:23 +0530

    skill form updated

--- app/(main)/editor/forms/skillform.tsx
+++ app/(main)/editor/forms/skillform.tsx
@@ -1,15 +1,30 @@
 import { EditorFormProps } from "@/lib/types";
 import { skillSchema, skillType } from "@/lib/validation";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
 
 export default function SkillForm({ResumeData,setResumeData}:EditorFormProps){
 
-const form = useform<skillType>({
+const form = useForm<skillType>({
     resolver:zodResolver(skillSchema),
     defaultValues:{
         skills:ResumeData.skills || [],
     },
 })
+ useEffect(() => {
+    const { unsubscribe } = form.watch(async (values) => {
+      const isValid = await form.trigger();
+      if (!isValid) return;
+      setResumeData({
+        ...ResumeData,
+       skills:values.skills
+       ?.filter( skill => skill!==undefined)
+       .map(skill => skill.trim())
+      });
+    });
+    return unsubscribe;
+  }, [form, ResumeData, setResumeData]);
 
 
 
