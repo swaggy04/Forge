e50161ff8e9f735b209142993a03c0a15a81e6d7
swagatpadhi <litan3104@gmail.com>
2026-05-27 17:19:10 +0530

    skill form updated

--- app/(main)/editor/forms/skillform.tsx
+++ app/(main)/editor/forms/skillform.tsx
@@ -4,36 +4,39 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 
-export default function SkillForm({ResumeData,setResumeData}:EditorFormProps){
-
-const form = useForm<skillType>({
-    resolver:zodResolver(skillSchema),
-    defaultValues:{
-        skills:ResumeData.skills || [],
+export default function SkillForm({
+  ResumeData,
+  setResumeData,
+}: EditorFormProps) {
+  const form = useForm<skillType>({
+    resolver: zodResolver(skillSchema),
+    defaultValues: {
+      skills: ResumeData.skills || [],
     },
-})
- useEffect(() => {
+  });
+  useEffect(() => {
     const { unsubscribe } = form.watch(async (values) => {
       const isValid = await form.trigger();
       if (!isValid) return;
       setResumeData({
         ...ResumeData,
-       skills:values.skills
-       ?.filter( skill => skill!==undefined)
-       .map(skill => skill.trim())
-       .filter(skill=>skill!=="")
+        skills: values.skills
+          ?.filter((skill) => skill !== undefined)
+          .map((skill) => skill.trim())
+          .filter((skill) => skill !== ""),
       });
     });
     return unsubscribe;
   }, [form, ResumeData, setResumeData]);
 
-
-
-
-    return(
-        <div>
-
-        </div>
-    )
-
+  return (
+    <div className="mx-auto space-y-6 max-w-xl">
+      <div className="space-y-1.5 text-center">
+        <h1 className="text-2xl font-bold">Education</h1>
+        <p className="text-muted-foreground text-sm">
+          add your work Educations{" "}
+        </p>
+      </div>
+    </div>
+  );
 }
