679f435d7eab941f577da03120e09fbd0fa9281e
swagatpadhi <litan3104@gmail.com>
2026-05-29 00:21:02 +0530

    previw page comp

--- components/previewpage.tsx
+++ components/previewpage.tsx
@@ -1,3 +1,17 @@
-export default function PreviewPage(){
+import { cn } from "@/lib/utils"
+import { ResumeValues } from "@/lib/validation"
 
+interface PreviewPageProps {
+    resumedata:ResumeValues
+    classname?:string
+
+}
+
+
+export default function PreviewPage({resumedata,classname}:PreviewPageProps){
+    return <div className= {cn("bg-white text-black h-fit w-full aspect-[210/297]",classname)}>
+        <h1 className="p-6 font-bold text-3xl">
+                this text should change color with the div
+        </h1>
+    </div>
 }
