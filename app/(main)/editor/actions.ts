92f126e9c66504d0aaaf1cc31cd197cedd411d84
swagatpadhi <litan3104@gmail.com>
2026-06-05 15:20:27 +0530

    action is added:-saving

--- app/(main)/editor/actions.ts
+++ app/(main)/editor/actions.ts
@@ -2,6 +2,8 @@
 import { prisma } from "@/lib/prisma";
 import { resumeSchema, ResumeValues } from "@/lib/validation";
 import { auth } from "@clerk/nextjs/server";
+import {del, put} from "@vercel/blob"
+import path from "node:path";
 
 export async function saveResume(values:ResumeValues){
     const {id} = values;
@@ -18,5 +20,16 @@ export async function saveResume(values:ResumeValues){
     : null
     if(id && !exixtingResume){
         throw new Error("Resume not found")
+    }
+    let newPhotoUrl: undefined | null | string = undefined 
+
+    if (photo) {
+        if(exixtingResume?.photoUrl){
+            await del(exixtingResume.photoUrl)
+        }
+        const blob = await put(`resume_photos/${path.extname(photo.name)}`,photo,{
+            access:"public"
+        })
+        newPhotoUrl = blob.url
     }
 }
