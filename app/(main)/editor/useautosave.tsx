e3389c8e2de9f522df33ec3da04e8f9bbb690862
swagatpadhi <litan3104@gmail.com>
2026-06-08 15:25:22 +0530

    action file changes and autosave changes

--- app/(main)/editor/useautosave.tsx
+++ app/(main)/editor/useautosave.tsx
@@ -6,86 +6,95 @@ import { useEffect, useState } from "react";
 import { resume } from "react-dom/server";
 import { saveResume } from "./actions";
 import { Button } from "@/components/ui/button";
+import { fileReplacer } from "@/lib/utils";
 
 export default function useAutoSaveResume(resumeData: ResumeValues) {
-
-  const searchParams = useSearchParams()
-  const{toast}= useToast()
+  const searchParams = useSearchParams();
+  const { toast } = useToast();
 
   const debouncedResumeData = useDebounce(resumeData, 1500);
-  const [resumeId,setResumeId] = useState(resumeData.id)
+  const [resumeId, setResumeId] = useState(resumeData.id);
 
   const [lastSavedData, setLastSavedData] = useState(
     structuredClone(resumeData),
   );
 
   const [isSaving, setIsSaving] = useState(false);
-  const[isError,setIsError] = useState(false)
+  const [isError, setIsError] = useState(false);
 
-  useEffect(()=>{
-    setIsError(false)
-  },[debouncedResumeData])
+  useEffect(() => {
+    setIsError(false);
+  }, [debouncedResumeData]);
 
   useEffect(() => {
     async function save() {
       try {
-        setIsSaving(true)
-        setIsError(false)
-        const newData = structuredClone(debouncedResumeData)
+        setIsSaving(true);
+        setIsError(false);
+        const newData = structuredClone(debouncedResumeData);
         const updatedResume = await saveResume({
           ...newData,
-          ...(lastSavedData.photo?.toString()=== newData.photo?.toString() && {
-            photo:undefined,
+          ...(JSON.stringify(lastSavedData.photo,fileReplacer) === JSON.stringify(newData.photo,fileReplacer) && {
+            photo: undefined,
           }),
-          id: resumeId
-        })
-        setResumeId(updatedResume.id)
-        setLastSavedData(newData)
+          id: resumeId,
+        });
+        setResumeId(updatedResume.id);
+        setLastSavedData(newData);
 
-        if(searchParams.get("resumeId")!== updatedResume.id){
-          const newSearchParams = new URLSearchParams(searchParams)
-          newSearchParams.set("resumeID",updatedResume.id)
+        if (searchParams.get("resumeId") !== updatedResume.id) {
+          const newSearchParams = new URLSearchParams(searchParams);
+          newSearchParams.set("resumeID", updatedResume.id);
           window.history.replaceState(
-            null,"",`?${newSearchParams.toString()}`
-          )
+            null,
+            "",
+            `?${newSearchParams.toString()}`,
+          );
         }
       } catch (error) {
-  setIsError(true);
+        setIsError(true);
 
-  console.error(error);
+        console.error(error);
 
-  const { dismiss } = toast({
-    variant: "destructive",
-    description: (
-      <div className="space-y-3">
-        <p>Could not save changes.</p>
+        const { dismiss } = toast({
+          variant: "destructive",
+          description: (
+            <div className="space-y-3">
+              <p>Could not save changes.</p>
 
-        <Button
-        variant="secondary"
-          onClick={() => {
-            dismiss();
-            save();
-          }}
-        >
-          Retry
-        </Button>
-      </div>
-    ),
-  });
-}finally{
-  setIsSaving(false)
-}
-
-      
+              <Button
+                variant="secondary"
+                onClick={() => {
+                  dismiss();
+                  save();
+                }}
+              >
+                Retry
+              </Button>
+            </div>
+          ),
+        });
+      } finally {
+        setIsSaving(false);
+      }
     }
+    console.log("debouncedResumeData",JSON.stringify(debouncedResumeData,fileReplacer))
+    console.log("lastSavedData",JSON.stringify(lastSavedData,fileReplacer))
 
     const hasUnsavedChanges =
-      JSON.stringify(debouncedResumeData) !== JSON.stringify(lastSavedData);
+      JSON.stringify(debouncedResumeData,fileReplacer) !== JSON.stringify(lastSavedData,fileReplacer);
 
     if (hasUnsavedChanges && debouncedResumeData && !isSaving && !isError) {
       save();
     }
-  }, [debouncedResumeData, isSaving, lastSavedData,isError,resumeId,searchParams]);
+  }, [
+    debouncedResumeData,
+    isSaving,
+    lastSavedData,
+    isError,
+    resumeId,
+    searchParams,
+  ]);
 
   return {
     isSaving,
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,14 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function  fileReplacer(key:unknown,value:unknown){
+  return value instanceof File ?
+  {
+    name:value.name,
+    size:value.size,
+    type:value.type,
+    lastModified:value.lastModified
+  }: value
+
+}
