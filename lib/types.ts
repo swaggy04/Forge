cdb6d50f3acb6635784cc262668926b1b5266593
swagatpadhi <litan3104@gmail.com>
2026-06-08 15:57:23 +0530

    type added

--- lib/types.ts
+++ lib/types.ts
@@ -1,3 +1,4 @@
+import { Prisma } from "@prisma/client";
 import { ResumeValues } from "./validation";
 
 export interface EditorFormProps {
@@ -5,3 +6,11 @@ export interface EditorFormProps {
     setResumeData: (data:ResumeValues) => void;
 
 }
+export const resumeDataInclude = {
+  workExperiences: true,
+  educations: true,
+} satisfies Prisma.ResumeInclude;
+
+export type ResumeServerData = Prisma.ResumeGetPayload<{
+  include: typeof resumeDataInclude;
+}>;
