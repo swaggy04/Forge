4e2cb4571f53db03f2981d5458bb09d47d569381
swagatpadhi <litan3104@gmail.com>
2026-06-08 15:47:03 +0530

    res preview

--- app/(main)/editor/page.tsx
+++ app/(main)/editor/page.tsx
@@ -1,5 +1,25 @@
-import ResumeEditor from "./resumeeditor/page";
 
-export default function Page(){
+import { auth } from "@clerk/nextjs/server";
+import ResumeEditor from "./resumeeditor/page";
+import { prisma } from "@/lib/prisma";
+interface PageProps{
+    searchParams:Promise<{resumeId?:string}>
+}
+
+export default async function  Page({searchParams}:PageProps){
+
+    const {resumeId} = await searchParams
+
+    const {userId} = await auth();
+
+    if(!userId){
+        return null;
+    }
+    const resumeToEdit = resumeId
+    ? await prisma.resume.findUnique({
+        where:{id:resumeId,userId}
+    })
+    :null
+
     return <ResumeEditor/>
 }
