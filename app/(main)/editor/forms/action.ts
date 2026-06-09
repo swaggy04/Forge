659554f67f7d35f9db0f31fa333af02b6a3ef0e8
swagatpadhi <litan3104@gmail.com>
2026-06-09 12:09:56 +0530

    ai feature

--- app/(main)/editor/forms/action.ts
+++ app/(main)/editor/forms/action.ts
@@ -13,7 +13,21 @@ export  async function generateSummary(input:GenerateSummaryValues){
     
     const userMessage =`please generate professional summary for this data
 
-    
+    Job title :${jobTitle || "N/A"}
+    Work experience :${
+        workexp?.map((exp)=>`
+            Position:${exp.position||"N/A"} at ${exp.company || "N/A"} from ${exp.startDate || "N/A"} to ${exp.endDate || "Present"}
+            Description:${exp.description || "N/A"}
+        `).join("\n\n")
+    }
 
+    Education :${
+        educations?.map((edu)=>`
+            Degree:${edu.degree||"N/A"} at ${edu.school || "N/A"} from ${edu.startDate || "N/A"} to ${edu.endDate || "N/A"}
+          
+        `).join("\n\n")
+        
+    }
+    SKills:${skills}
     `
 }
