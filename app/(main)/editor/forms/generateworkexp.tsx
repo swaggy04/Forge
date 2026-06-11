1c739c8918380f92f19dd390722eb25d43bb3ea5
swagatpadhi <litan3104@gmail.com>
2026-06-11 11:15:34 +0530

    update for ai feat

--- app/(main)/editor/forms/generateworkexp.tsx
+++ app/(main)/editor/forms/generateworkexp.tsx
@@ -1,16 +1,20 @@
-import LoadingButton from "@/components/loadingbutton";
+// import LoadingButton from "@/components/loadingbutton";
 import { useToast } from "@/hooks/usetoast";
 // import { ResumeValues } from "@/lib/validation";
 import { WandSparklesIcon } from "lucide-react";
 import { useState } from "react";
-import {  generateWorkExp } from "./action";
-import { workExperience } from "@/lib/validation";
+// import {  generateWorkExp } from "./action";
+import { generateWorkExpSchema, GenerateWorkExpValues, workExperience } from "@/lib/validation";
 import { Button } from "@/components/ui/button";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { generateWorkExp } from "./action";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 
 
 interface WorkExpGenerationButtonProps{
     
-    onGeneratedDescription:(workExperience:workExperience)=> void
+    onGeneratedDescription:(description:string)=> void
 }
 
 export default function GenerateSummaryButton({onGeneratedDescription}:WorkExpGenerationButtonProps){
@@ -32,6 +36,46 @@ export default function GenerateSummaryButton({onGeneratedDescription}:WorkExpGe
 interface InputDialogProps{
     open:boolean,
     onOpenChange: (open:boolean)=>void,
-    onGeneratedDescription:(workExperience:workExperience)=> void
+    onGeneratedDescription:(description:string)=> void
+
+}
+
+function InputDialog({open,onOpenChange,onGeneratedDescription}:InputDialogProps){
+    const{toast} =useToast()
+
+    const form = useForm<GenerateWorkExpValues>({
+        resolver:zodResolver(generateWorkExpSchema),
+        defaultValues:{
+            description:""
+        }
+    })
+
+    async function onSubmit(input:GenerateWorkExpValues){
+        try {
+            const response = await generateWorkExp(input)
+            onGeneratedDescription(response)
+            
+        } catch (error) {
+            console.error(error)
+             toast({
+                variant:"destructive",
+                description:"something went wrong please try again later "
+            })
+        }
+    }
+    return (
+        <Dialog>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>
+                        Generate Work Experience
+                    </DialogTitle>
+                    <DialogDescription>
+                        Describe the work experience and ai will improve it for you
+                    </DialogDescription>
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>
+    )
 
 }
