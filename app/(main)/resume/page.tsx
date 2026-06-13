fcca79a7537f58d51243787a1e58f4359f1754a9
swagatpadhi <litan3104@gmail.com>
2026-06-13 15:15:48 +0530

    fetch feat

--- app/(main)/resume/page.tsx
+++ app/(main)/resume/page.tsx
@@ -6,33 +6,34 @@ import { auth } from "@clerk/nextjs";
 import { FilePlus } from "lucide-react";
 
 import Link from "next/link";
+import { promise } from "zod";
 
 export default async function page() {
-
-
-  const {userId} = await auth
-  if(!userId){
-    return null
+  const { userId } = await auth;
+  if (!userId) {
+    return null;
   }
 
-  const resume = await prisma.resume.findMany({
-    where:{
-      userId
-    },
-    orderBy:{
-      updatedAt:"desc"
-    },
-    include:resumeDataInclude
-  })
-
-  
-
+  const [resume, totalCount] = await Promise.all([
+    prisma.resume.findMany({
+      where: {
+        userId,
+      },
+      orderBy: {
+        updatedAt: "desc",
+      },
+      include: resumeDataInclude,
+    }),
+    prisma.resume.count({
+      where: userId,
+    }),
+  ]);
 
   return (
     <main className="mx-auto w-full max-w-7xl space-y-6 px-3 py-4">
       <Button asChild className="mx-auto w-fit flex gap-2 bg-slate-950">
         <Link href="/editor">
-        <FilePlus className="size-5"/>
+          <FilePlus className="size-5" />
           Create Resume
         </Link>
       </Button>
