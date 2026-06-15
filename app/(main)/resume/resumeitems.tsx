1b202b145581362cbddb555df0b4238ddf55724f
swagatpadhi <litan3104@gmail.com>
2026-06-15 11:37:59 +0530

    fetch feat

--- app/(main)/resume/resumeitems.tsx
+++ app/(main)/resume/resumeitems.tsx
@@ -1,1 +1,15 @@
 "use client"
+
+import { ResumeServerData } from "@/lib/types"
+
+interface ResumeItemsProps{
+    resume:ResumeServerData
+}
+
+export function resumeItems({resume}:ResumeItemsProps){
+    const updatedResume = resume.updatedAt!== resume.createdAt
+
+    return <div>
+        resume item
+    </div>
+}
