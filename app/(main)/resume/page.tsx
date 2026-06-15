351b56cc773399b28c373ed8e8f243875297f721
swagatpadhi <litan3104@gmail.com>
2026-06-15 13:30:58 +0530

    fetch feat

--- app/(main)/resume/page.tsx
+++ app/(main)/resume/page.tsx
@@ -1,21 +1,19 @@
 import { Button } from "@/components/ui/button";
 import { prisma } from "@/lib/prisma";
 import { resumeDataInclude } from "@/lib/types";
-import { auth } from "@clerk/nextjs";
-
 import { FilePlus } from "lucide-react";
-
 import Link from "next/link";
-import { promise } from "zod";
 import ResumeItems from "./resumeitems";
+import { auth } from "@clerk/nextjs/server";
 
-export default async function page() {
-  const { userId } = await auth;
+export default async function Page() {
+  const { userId } = await auth();
+
   if (!userId) {
     return null;
   }
 
-  const [resume, totalCount] = await Promise.all([
+  const [resumes, totalCount] = await Promise.all([
     prisma.resume.findMany({
       where: {
         userId,
@@ -25,32 +23,38 @@ export default async function page() {
       },
       include: resumeDataInclude,
     }),
+
     prisma.resume.count({
-      where: userId,
+      where: {
+        userId,
+      },
     }),
   ]);
 
   return (
     <main className="mx-auto w-full max-w-7xl space-y-6 px-3 py-4">
-      <Button asChild className="mx-auto w-fit flex gap-2 bg-slate-950">
+      <Button asChild className="w-fit bg-slate-950">
         <Link href="/editor">
           <FilePlus className="size-5" />
           Create Resume
         </Link>
-        <div className="space-y-1">
-          <h1 className="font-bold text-3xl">Your resume</h1>
-          <p>Total:{totalCount}</p>
-        </div>
-        <div className="flex flex-col sm:grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 w-full gap-3">
-          
-            {resume.map((resume) =>(
-              <ResumeItems
-              key={resume.id}
-              resume={resume}
-              />
-            ))}
-        </div>
       </Button>
+
+      <div className="space-y-1">
+        <h1 className="text-3xl font-bold">
+          Your Resumes
+        </h1>
+        <p>Total: {totalCount}</p>
+      </div>
+
+      <div className="grid w-full grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {resumes.map((resume) => (
+          <ResumeItems
+            key={resume.id}
+            resume={resume}
+          />
+        ))}
+      </div>
     </main>
   );
 }
