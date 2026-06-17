5caf88a4a0bbc651d868c4f76decb4af0e54c898
swagatpadhi <litan3104@gmail.com>
2026-06-17 15:34:58 +0530

    fetch feat

---
+++ app/(main)/resume/actions.ts
@@ -0,0 +1,18 @@
+"use server"
+
+import { prisma } from "@/lib/prisma"
+import { auth } from "@clerk/nextjs/server"
+
+export  async function deleteResume(id:string){
+    const {userId} = await auth()
+
+    if(!userId){
+        throw new Error ("user not authenticated")
+    }
+    const resume = await prisma.resume.findUnique({
+        where:{
+            id,
+            userId
+        }
+    })
+}
